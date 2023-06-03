2b8b26d899ef8bb93cb1060523d63bd47a04df1d
Diego Fleitas <diego.fleitas68@gmail.com>
2023-06-03 17:48:34 -0300

    refactor vuex store

---
+++ src/searches.js
@@ -0,0 +1,120 @@
+import { performFetch, getSteamId, isCacheExpired } from "./utils.js";
+import serversData from "./servers.json";
+import localForage from "localforage";
+import SteamId from "steamid";
+
+export default {
+  state: {
+    searches: serversData.servers.map((server) => ({
+      domain: server.domain,
+      url: server.url,
+      status: null,
+      result: null,
+    })),
+    steamId: "",
+    serversChecked: 0,
+    totalServers: serversData.servers.length,
+  },
+  mutations: {
+    updateSearch(state, { domain, data }) {
+      const searchIndex = state.searches.findIndex(
+        (search) => search.domain === domain
+      );
+      if (searchIndex !== -1) {
+        state.searches[searchIndex] = {
+          ...state.searches[searchIndex],
+          ...data,
+        };
+      }
+    },
+    clearSearches(state) {
+      state.searches = serversData.servers.map((server) => ({
+        domain: server.domain,
+        url: server.url,
+        status: null,
+        result: null,
+      }));
+    },
+
+    incrementServersChecked(state) {
+      state.serversChecked++;
+    },
+    clearServersChecked(state) {
+      state.serversChecked = 0;
+    },
+  },
+  actions: {
+    // async performSearch({ commit }, steamId) {...},
+    async performSearch({ commit, state }, steamId) {
+      commit("clearServersChecked");
+      const servers = serversData.servers;
+      const proxy = "https://stark-woodland-93683.fly.dev/";
+      let player = new SteamId(steamId);
+      servers.forEach(async (server) => {
+        const domain = server.domain;
+        const steamId = getSteamId(player, server.steamIdType);
+        const url = `${proxy}${server.url}${steamId}`;
+
+        const cacheKey = `${steamId}_${domain}`;
+        const cachedData = await localForage.getItem(cacheKey);
+
+        if (cachedData && !isCacheExpired(cachedData.timestamp)) {
+          commit("updateSearch", {
+            domain,
+            data: {
+              url,
+              status: "complete",
+              result: cachedData.result,
+            },
+          });
+          commit("incrementServersChecked");
+          return;
+        }
+
+        commit("updateSearch", {
+          domain,
+          data: { url, status: "loading", result: null },
+        });
+
+        try {
+          const result = await performFetch({
+            url,
+            xpath: server.selector,
+            selectorIndex: server.selectorIndex,
+            selectorText: server?.selectorText,
+          });
+          await localForage.setItem(cacheKey, {
+            result,
+            timestamp: Date.now(),
+          });
+          commit("updateSearch", {
+            domain,
+            data: { status: "complete", result },
+          });
+        } catch (error) {
+          commit("updateSearch", {
+            domain,
+            data: { status: "error", result: error.toString() },
+          });
+        } finally {
+          commit("incrementServersChecked");
+        }
+      });
+    },
+    clearSearches({ commit }) {
+      commit("clearSearches");
+    },
+  },
+  getters: {
+    sortedSearches: (state) => {
+      const searches = [...state.searches];
+
+      return searches.sort((a, b) => {
+        const statuses = ["Banned", "Not banned", "loading", "error"];
+        return statuses.indexOf(a.result) - statuses.indexOf(b.result);
+      });
+    },
+    progressCount: (state) =>
+      `[${state.serversChecked} / ${state.totalServers} servers being checked]`,
+  },
+};
---
+++ src/testResults.js
@@ -0,0 +1,39 @@
+import { performFetch } from "./utils.js";
+import serversData from "./servers.json";
+
+export default {
+  state: {
+    testResults: {},
+  },
+  mutations: {
+    updateTestResult(state, { domain, result }) {
+      state.testResults[domain] = result;
+    },
+  },
+  actions: {
+    // testSearch({ commit }, domain) {...},
+    testSearch({ commit }, domain) {
+      const server = serversData.servers.find((sv) => sv.domain === domain);
+      const proxy = "https://stark-woodland-93683.fly.dev/";
+      const testUrl = proxy + server.example;
+      performFetch({
+        url: testUrl,
+        xpath: server.selector,
+        selectorIndex: server.selectorIndex,
+        selectorText: server?.selectorText,
+      })
+        .then((banStatus) => {
+          const result = banStatus === "Banned" ? "pass" : "fail";
+          commit("updateTestResult", { domain, result });
+        })
+        .catch(() => {
+          commit("updateTestResult", { domain, result: "fail" });
+        });
+    },
+  },
+  getters: {
+    getTestResult: (state) => {
+      return state.testResults;
+    },
+  },
+};
